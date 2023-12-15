d4fb5d728d6917be4d2579acaf7163d863231308
kutang <zheng-yimeng@outlook.com>
2023-12-15 23:02:31 +0800

    修改代码

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,41 @@
-import { BrowserProvider, Contract } from "ethers";
+import { BrowserProvider, Contract, Wallet } from "ethers";
 import ABI from "./abi.json";
 
-const provider = new BrowserProvider(window.ethereum);
+const provider = new BrowserProvider(window.ethereum, 0x406);
 
 const contractAddress = "0xc6e865c213c89ca42a622c5572d19f00d84d7a16";
 const contract = new Contract(contractAddress, ABI, provider);
 
+const input = document.createElement("input");
+input.placeholder = "钱包私钥";
+input.style.cssText = "width: 300px; height: 44px";
+document.body.append(input);
+
 const button = document.createElement("button");
 button.innerHTML = "click mint";
+button.style.cssText = "height: 44px";
 document.body.append(button);
 
 button.addEventListener("click", async () => {
-  console.log("mint start");
-  const signer = contract.connect(await provider.getSigner()) as Contract;
-  const result = await signer.CreateCFXs();
-  console.log(result);
-  await result?.wait();
-  console.log("wait success");
+  const wallet = new Wallet(input.value, provider);
+
+  let count = 1;
+  const mintCfx = async () => {
+    console.log("mint start ------", count);
+    const signer = contract.connect(wallet) as Contract;
+    const result = await signer.CreateCFXs();
+    await result?.wait();
+    console.log("success", count);
+  };
+
+  while (true) {
+    try {
+      await mintCfx();
+    } catch (error) {
+      console.log(error.message);
+      console.log("error", count);
+    } finally {
+      count += 1;
+    }
+  }
 });
